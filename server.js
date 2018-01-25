d1a82451b420941b89c672334a37ce2861464612
Ryan Warsaw <email@ryan-w.me>
2018-01-25 15:14:49 -0600

    Implement .env functionality, Replace README contents with project details

---
+++ server.js
@@ -0,0 +1,5 @@
+const dotenv = require("dotenv").config();
+const { IncomingWebhook, WebClient } = require("@slack/client");
+
+const client = new WebClient(process.env.SLACK_TOKEN);
+const webhook = new IncomingWebhook(process.env.SLACK_WEB_HOOK_URL);
