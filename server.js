632571bb520b9432dd73019329a0572979ea9bfe
Ryan Warsaw <email@ryan-w.me>
2018-01-25 16:27:19 -0600

    Add curriculum tools, Update .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv").config();
 const { IncomingWebhook, WebClient } = require("@slack/client");
+const curriculum = require("curriculum-tools");
 
 const client = new WebClient(process.env.SLACK_TOKEN);
 const webhook = new IncomingWebhook(process.env.SLACK_WEB_HOOK_URL);
