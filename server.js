c30bc0f0909773e0ea74abf649fadfd32c29a1b9
Ryan Warsaw <email@ryan-w.me>
2018-01-25 21:44:03 -0600

    Remove source files from root project directory

--- server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv").config();
-const { IncomingWebhook, WebClient } = require("@slack/client");
-const { Curriculum, GitHub } = require("curriculum-tools").content;
-
-const client = new WebClient(process.env.SLACK_TOKEN);
-const webhook = new IncomingWebhook(process.env.SLACK_WEB_HOOK_URL);
-const curriculum = new Curriculum(new GitHub("submodules"));
-
-console.log(curriculum.getStats());
