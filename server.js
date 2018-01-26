7b2295a3bcc6d3dedfab334e3b379314dcd3fce1
Ryan Warsaw <email@ryan-w.me>
2018-01-25 19:59:59 -0600

    Migrate over to git submodules for curriculum management

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { Curriculum, GitHub } = require("curriculum-tools").content;
 
 const client = new WebClient(process.env.SLACK_TOKEN);
 const webhook = new IncomingWebhook(process.env.SLACK_WEB_HOOK_URL);
-const curriculum = new Curriculum(new GitHub(process.argv[2]));
-// TODO: Get rid of this, instead use git sub-modules locally.
+const curriculum = new Curriculum(new GitHub("submodules"));
 
 console.log(curriculum.getStats());
