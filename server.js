f5aeb6724ca06abfb814674ea29adfb88190bda9
Ryan Warsaw <email@ryan-w.me>
2018-01-25 19:54:38 -0600

    Add curriculum, standards submodules for curriculum-tools dependency

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const dotenv = require("dotenv").config();
 const { IncomingWebhook, WebClient } = require("@slack/client");
-const curriculum = require("curriculum-tools");
+const { Curriculum, GitHub } = require("curriculum-tools").content;
 
 const client = new WebClient(process.env.SLACK_TOKEN);
 const webhook = new IncomingWebhook(process.env.SLACK_WEB_HOOK_URL);
+const curriculum = new Curriculum(new GitHub(process.argv[2]));
+// TODO: Get rid of this, instead use git sub-modules locally.
+
+console.log(curriculum.getStats());
