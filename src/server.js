59844258e26c783af33f6be9a616624b510dac3f
Ryan Warsaw <email@ryan-w.me>
2018-01-25 22:09:44 -0600

    Add express server and automatically serve landing page

--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
 const dotenv = require("dotenv").config();
 const { IncomingWebhook, WebClient } = require("@slack/client");
 const { Curriculum, GitHub } = require("curriculum-tools").content;
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const client = new WebClient(process.env.SLACK_TOKEN);
 const webhook = new IncomingWebhook(process.env.SLACK_WEB_HOOK_URL);
 const curriculum = new Curriculum(new GitHub("submodules"));
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  // Serve an asset here, like an HTML page.
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`🍃  Parakeet web services are now operational on PORT ${process.env.PORT}`);
+
+  if (process.env.SLACK_TOKEN) {
+    console.log(`🍃  Parakeet slack integration is now operational.`);
+    // TODO: Initialize the bot, start accepting user interaction.
+  }
+})
 
 console.log(curriculum.topics);
 
